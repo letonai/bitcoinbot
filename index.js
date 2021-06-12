67a948539430da39b8b8b68d0cee1efc72a1d75e
letonai <letonai@gmail.com>
2021-06-12 16:08:36 -0300

    Dev (#1)
    * New menu config
    
    * Docker file
    
    * Fixed menu load
    
    * teste
    
    * Update Dockerfile
    
    Co-authored-by: ricardo.letonai-ext@apontador.com <ricardo.letonai-ext@apontador.com>

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-//CI TEST7
 const request = require('request-promise')
 const Telegraf = require('telegraf');
 const { reply } = Telegraf;
 const { Markup,Extra } = Telegraf
-
-
 var fs = require('fs');
+const { exit } = require('process');
 
-var obj = JSON.parse(fs.readFileSync('./apikey.secret', 'utf8'));
-const bot = new Telegraf(obj.secret);
+var key = JSON.parse(fs.readFileSync('./apikey.secret', 'utf8'));
+var menuConfig = JSON.parse(fs.readFileSync('./menu.conf', 'utf8'));
+registerMenus(menuConfig);
+
+const bot = new Telegraf(key.secret);
 console.log(process.version.match(/^v(\d+\.\d+)/)[1])
-
-
 
 bot.startPolling();
 
@@ -27,14 +26,6 @@ bot.command('/cota',(ctx) => cota(ctx) );
 bot.on('callback_query',ctx => {
     coinRequested = ctx.update.callback_query.data;
     request("http://localhost:8990/"+coinRequested).then(function (body) {
-      // console.log(body);
-      // let coin=JSON.parse(body);
-      // var coinValue=parseFloat(coin[coinRequested].usd);
-      // if(coinValue<0.01){
-      //   coinValue=parseFloat(coinValue).toFixed(8);
-      // }else{
-      //   coinValue=parseFloat(coinValue).toFixed(2);
-      // }
       ctx.reply(body+" USD");
     }).catch(function (err) {
       console.error("Erro XXX: "+err);
@@ -42,10 +33,16 @@ bot.on('callback_query',ctx => {
  
   });
 
-function cota(ctx){
+function registerMenus(jsonConfig){
   let  btMenu = [];
-  btMenu.push(Markup.callbackButton("BTC", 'bitcoin'));
-  btMenu.push(Markup.callbackButton("XRP", 'ripple'));
-  btMenu.push(Markup.callbackButton("LTC", 'litecoin'));
-  ctx.reply("Moedas:",Markup.inlineKeyboard(btMenu).extra());
+  jsonConfig["menu"].forEach(element => {
+   btMenu.push(Markup.callbackButton(element.symbol, element.name));
+   //teste
+  });
+  return btMenu;
+}
+
+
+function cota(ctx){
+  ctx.reply("Moedas:",Markup.inlineKeyboard(registerMenus(menuConfig)).extra());
 }
